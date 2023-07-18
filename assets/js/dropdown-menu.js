5fbd7dcb0041b30d7f811cdcd391cb8eb5b41bc9
Antoine Cadoret <antoinecadoret97@gmail.com>
2023-07-18 18:28:48 +0200

    [build(dropdown-menu.js)] some modification

--- assets/js/dropdown-menu.js
+++ assets/js/dropdown-menu.js
@@ -3,11 +3,11 @@ button.addEventListener('click', (event) => {
 console.log('test');
 if(document.querySelector('#dropdown-button').className === 'close'){
     document.querySelector('#dropdown-button').className = 'open';
-    document.getElementById('dropdown-icon').src = './images/more.png';
-    document.querySelector('.dropdown-nav').style.display = 'none';
-}else{
-    document.querySelector('#dropdown-button').className = 'close';
     document.getElementById('dropdown-icon').src = './images/arrow.png';
     document.querySelector('.dropdown-nav').style.display = 'flex';
+}else{
+    document.querySelector('#dropdown-button').className = 'close';
+    document.getElementById('dropdown-icon').src = './images/more.png';
+    document.querySelector('.dropdown-nav').style.display = 'none';
 }
 });
